5b5dc695b928ff04bac44f80f50a3454a0a473b0
Dajuan Mcdonald <16410995+FIOpwK@users.noreply.github.com>
2020-09-14 23:28:36 -0400

    fixed robot.py to pass testing

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -97,7 +97,39 @@ class SortingRobot:
         Sort the robot's list.
         """
         # Fill this out
-        pass
+        
+        while self.light_is_on() == True:
+            if self.compare_item() == None:
+                self.swap_item()
+
+            while self.can_move_right() == True:
+                self.move_right()
+
+                if self.compare_item() == -1 and self.can_move_right() == True:
+                    self.swap_item()
+
+                elif self.can_move_right() == False and self.compare_item() == 1:
+                    self.swap_item()
+
+            while self.can_move_left() == True:
+                self.move_left()
+
+                if self.compare_item() == 1:
+                    self.swap_item()
+
+                elif self.compare_item() == None and self.can_move_right() == True:
+
+                    self.swap_item()
+                    self.move_right()
+
+                    if self.can_move_right() == False and self.compare_item() == None:
+                        self.set_light_off()
+
+                    break
+
+                elif self.compare_item() == None and self.can_move_right() == False:
+                    self.set_light_off()
+                    break
 
 
 if __name__ == "__main__":
